45d870906d055b1270bef5f136dd7b3d3c4c4bd6
Arkadiusz Kuryłowicz <arkadiusz@kurylowicz.info>
2021-10-02 00:36:32 +0200

    Additional IP handling

--- src/Serwisant/SerwisantCp/Api.php
+++ src/Serwisant/SerwisantCp/Api.php
@@ -11,8 +11,7 @@ class Api extends \Serwisant\SerwisantApi\Api
   public function __construct(Silex\Application $app, Request $request, $access_token)
   {
     $this
-      ->setIp($request->getClientIp())
-      ->setLang($request->headers->get('Accept-Language'))
+      ->setUpFromRequest($request)
       ->setAccessToken($access_token);
 
     foreach ($app['gql_query_paths'] as $path) {
